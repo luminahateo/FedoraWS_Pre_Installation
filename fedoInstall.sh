55b8f06990fed21d5e83ba3b655e40ace153ca93
Hateo Lumina <29329710+luminahateo@users.noreply.github.com>
2021-05-15 13:14:01 +0200

    Mise à jour
    Migration fait vers gnome 40 // Fedora 34

--- fedoInstall.sh
+++ fedoInstall.sh
@@ -3,11 +3,12 @@
 #INSTALLATION FEDORA
 ####################################################################################################################################################################################################################################################################
 
+#PreInstall
 sudo dnf update -y
-
 sudo dnf install --nogpgcheck https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm && sudo dnf install --nogpgcheck https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo dnf update
+sudo dnf update -y
+echo "PréInstallation OK" >> .suivisInstall.txt
 
 #Codecs
 sudo dnf install rpmfusion-free-appstream-data && sudo dnf install rpmfusion-nonfree-appstream-data
@@ -15,33 +16,76 @@ sudo dnf install gstreamer-ffmpeg gstreamer-plugins-bad gstreamer-plugins-bad-no
 sudo dnf install gstreamer1-plugins-{base,good,bad-free,good-extras,bad-free-extras} gstreamer1-plugin-mpg123
 sudo dnf install gstreamer1-libav gstreamer1-plugins-{bad-freeworld,ugly}
 
-#Logiciels
+#Logiciels_DNF
 ####################################################################################################################################################################################################################################################################
 #Bureautique
-sudo dnf install libreoffice*
-sudo dnf install https://github.com/Automattic/simplenote-electron/releases/download/v2.7.0/Simplenote-linux-2.7.0-x86_64.rpm
+sudo dnf install libreoffice* -y 
+echo "Logiciel_DNF Bureautique OK" >> .suivisInstall.txt
 
 #Graphisme
-sudo dnf install blender inkscape krita gpick
+sudo dnf install blender inkscape krita -y
+echo "Logiciel_DNF Graphisme OK" >> .suivisInstall.txt
 
 #internet
-sudo dnf install discord firefox* thunderbird* uget transmission
-sudo flatpak install tutanota
+sudo dnf install discord firefox thunderbird uget transmission -y 
+echo "Logiciel_DNF Internet OK" >> .suivisInstall.txt
 
 #Utilitaire
-sudo dnf install VirtualBox wine* cool-retro-term htop gparted
+sudo dnf install VirtualBox wine ytop gparted gnome-tweaks gnome-extensions-app -y 
 systemctl restart systemd-modules-load.service
-sudo dnf copr enable  tuxino/blanket
-sudo dnf install blanket
+echo "Logiciel_DNF Utilitaire OK" >> .suivisInstall.txt
 
 #Code
-sudo dnf install '/home/lumina/Téléchargements/atom.x86_64.rpm' '/home/lumina/Téléchargements/gitkraken-amd64.rpm'
-sudo dnf install godot*
+sudo dnf install godot -y
+echo "Logiciel_DNF Code OK" >> .suivisInstall.txt
 
-#Audio
-sudo dnf remove rhythmbox*
-sudo dnf install lollypop vlc* lpf-spotify-client soundconverter kdenlive tuxguitar
+#AudioVideo
+sudo dnf install vlc soundconverter kdenlive tuxguitar -y
+echo "Logiciel_DNF AndioVideo OK" >> .suivisInstall.txt
 
 #Jeux
-sudo dnf install steam minigalaxy openmw*
-flatpak install minecraft
+sudo dnf install steam minigalaxy openmw -y
+echo "Logiciel_DNF Jeux OK" >> .suivisInstall.txt
+
+#Logiciels_Flatpak
+####################################################################################################################################################################################################################################################################
+
+#Bureautique
+flatpak install Planner Dialect Foliate Simplenote Apostrophe -y
+flatpak install flathub com.github.junrrein.PDFSlicer
+flatpak install flathub com.github.taiko2k.tauonmb
+echo "Logiciel_Flatpak Bureautique OK" >> .suivisInstall.txt
+
+#Graphisme
+flatpak install Pixelorama Tutanota Natron Blender Identity Inkscape Kdenlive Krita Scribus -y
+flatpak install flathub nl.hjdskes.gcolor3
+echo "Logiciel_Flatpak Graphisme OK" >> .suivisInstall.txt
+
+#Internet
+flatpak install Discord Tor Thunderbird Tangram -y
+echo "Logiciel_Flatpak Internet OK" >> .suivisInstall.txt
+
+#Utilitaire
+flatpak install Flatseal -y
+echo "Logiciel_Flatpak Utilitaire OK" >> .suivisInstall.txt
+
+#Code
+flatpak install Atom Meld Godot -y
+echo "Logiciel_Flatpak Code OK" >> .suivisInstall.txt
+
+#AudioVideo
+flatpak install TuxGuitar flacon Curtail Blanket RawTherapee Spotify Shortwave FreeTube FamiStudio -y 
+flatpak install flathub com.bitwig.BitwigStudio -y
+echo "Logiciel_Flatpak AudioVideo OK" >> .suivisInstall.txt
+
+#Jeux
+flatpak install Minecraft Steam Minigalaxy OpenMW -y
+echo "Logiciel_Flatpak Jeux OK" >> .suivisInstall.txt
+
+
+#Theme Fedora
+####################################################################################################################################################################################################################################################################
+
+sudo dnf install 
+flatpak install flathub org.gtk.Gtk3theme.Yaru-Aqua-dark -y
+echo "theme Flatpak OK" >> .suivisInstall.txt
