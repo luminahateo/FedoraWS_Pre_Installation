f4c20c2cacc35120a59b05560336603a122ca27b
Hoffmann Julien <29329710+luminahateo@users.noreply.github.com>
2022-01-11 22:20:10 +0100

    Update extensions.sh

--- GNOME/extensions.sh
+++ GNOME/extensions.sh
@@ -4,9 +4,9 @@
 ################################################################################
 sudo dnf install -y gnome-shell-extension-pop-shell
 sudo dnf install -y gnome-shell-extension-pop-shell-shortcut-overrides
-# sudo dnf install -y gnome-shell-extension-appindicator
-# sudo dnf install -y gnome-shell-extension-sound-output-device-chooser
-# sudo dnf install -y gnome-shell-extension-user-theme
+sudo dnf install -y gnome-shell-extension-appindicator
+sudo dnf install -y gnome-shell-extension-sound-output-device-chooser
+sudo dnf install -y gnome-shell-extension-user-theme
 
 sudo dnf autoremove -y gnome-shell-extension-background-logo
 sudo dnf autoremove -y gnome-shell-extension-apps-menu
