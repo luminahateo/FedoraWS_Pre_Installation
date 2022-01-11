bbc5bbd718fe431641713ab0c9b13f5d6f5c4333
Hoffmann Julien <29329710+luminahateo@users.noreply.github.com>
2022-01-11 22:18:22 +0100

    Update themes.sh

--- GNOME/themes.sh
+++ GNOME/themes.sh
@@ -6,21 +6,21 @@ sudo dnf install -y materia-gtk-theme
 #sudo dnf install -y yaru*
 
 sudo cp -R Icons/kora Icons/kora-light Icons/kora-light-panel /usr/share/icons
-sudo cp -R Curseurs/oxy-neon /usr/share/icons
+# sudo cp -R Curseurs/oxy-neon /usr/share/icons
 
 gsettings set org.gnome.desktop.interface icon-theme "kora"
-gsettings set  org.gnome.desktop.interface cursor-theme "oxy-neon"
+# gsettings set  org.gnome.desktop.interface cursor-theme "oxy-neon"
 gsettings set org.gnome.desktop.interface gtk-theme "Materia-dark-compact"
 
 #wallpaper
 ################################################################################
-sudo cp Extra/FDSL.png /usr/share/backgrounds/
+sudo cp Extra/FDSL.jpg /usr/share/backgrounds/
 URI="file:///usr/share/backgrounds/FDSL.jpg"
 echo ${URI}
 
 #Parametre interface
 ################################################################################
-gsettings set org.gnome.desktop.background picture-options 'centered'
+gsettings set org.gnome.desktop.background picture-options 'spanned'
 gsettings set org.gnome.desktop.background picture-uri "${URI}"
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close:'
 
